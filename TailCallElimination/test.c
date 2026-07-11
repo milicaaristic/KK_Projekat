d9fcdb2ffbc8c723ab52e12e66e0b4337fae110c
milicaaristic <milicaristic1308@gmail.com>
2026-07-11 21:11:51 +0200

    Update test.c

--- TailCallElimination/test.c
+++ TailCallElimination/test.c
@@ -1,1 +1,12 @@
-// TODO: test primer za Tail Call Elimination.
+#include <stdio.h>
+
+int sum(int n, int acc) {
+    if (n == 0)
+        return acc;
+    return sum(n - 1, acc + n); 
+}
+
+int main() {
+    printf("%d\n", sum(100, 0)); 
+    return 0;
+}
